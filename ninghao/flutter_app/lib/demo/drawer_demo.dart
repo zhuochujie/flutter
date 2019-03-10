5c6b7b73400f3e234298b52d77ccaefc82f40763
Z大叔 <2662648243@qq.com>
2019-03-10 19:32:04 +0800

    first one

---
+++ ninghao/flutter_app/lib/demo/drawer_demo.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName:
+                Text('Z大叔', style: TextStyle(fontWeight: FontWeight.bold)),
+            accountEmail: Text('xiaoZV587@qq.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i2.hdslb.com/bfs/face/888c0271ab7e4612ab20cc65fbe9e026f0834d7e.jpg@72w_72h.webp'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.yellow[400],
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.yellow[400].withOpacity(0.6), BlendMode.hardLight),
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Messages',
+            ),
+            leading: Icon(Icons.message, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Favorite',
+            ),
+            leading: Icon(Icons.favorite, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+          ListTile(
+            title: Text(
+              'Settings',
+            ),
+            leading: Icon(Icons.settings, color: Colors.black12, size: 22.0),
+            onTap: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+}
